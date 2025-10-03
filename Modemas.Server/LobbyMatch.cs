375d3b94b4128cf7793d2580edeb6a8ef3ded0f0
someguy0555 <gjonas2005@gmail.com>
2025-10-04 02:46:26 +0300

    Added LobbyMatch and LobbySettings.cs

---
+++ Modemas.Server/LobbyMatch.cs
@@ -0,0 +1,8 @@
+namespace Modemas.Server
+{
+    public class LobbyMatch
+    {
+        public List<Question> Questions = [];
+        public int CurrentQuestionIndex = 0;
+    }
+}
---
+++ Modemas.Server/LobbySettings.cs
@@ -0,0 +1,8 @@
+namespace Modemas.Server
+{
+    public class LobbySettings
+    {
+        public int NumberOfQuestions = 10;
+        public int QuestionTimerInSeconds = 10;
+    }
+}
